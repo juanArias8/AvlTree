6ecb4e8388b2e4065a038fd69e8666bfcf09c64e
juanArias8 <juasda96@gmail.com>
2018-05-27 22:22:34 -0500

    try catch added

--- src/UserInterface/Main.java
+++ src/UserInterface/Main.java
@@ -7,23 +7,35 @@ public class Main {
     public static void main(String[] args) {
         AvlTree tree = new AvlTree();
 
-        tree.setRoot(tree.insertNode(tree.getRoot(), 9));
-        tree.setRoot(tree.insertNode(tree.getRoot(), 5));
-        tree.setRoot(tree.insertNode(tree.getRoot(), 10));
-        tree.setRoot(tree.insertNode(tree.getRoot(), 0));
-        tree.setRoot(tree.insertNode(tree.getRoot(), 6));
-        tree.setRoot(tree.insertNode(tree.getRoot(), 11));
-        tree.setRoot(tree.insertNode(tree.getRoot(), -1));
-        tree.setRoot(tree.insertNode(tree.getRoot(), 1));
-        tree.setRoot(tree.insertNode(tree.getRoot(), 2));
+        try {
+            tree.setRoot(tree.insertNode(tree.getRoot(), 9));
+            tree.setRoot(tree.insertNode(tree.getRoot(), 5));
+            tree.setRoot(tree.insertNode(tree.getRoot(), 10));
+            tree.setRoot(tree.insertNode(tree.getRoot(), 0));
+            tree.setRoot(tree.insertNode(tree.getRoot(), 6));
+            tree.setRoot(tree.insertNode(tree.getRoot(), 11));
+            tree.setRoot(tree.insertNode(tree.getRoot(), -1));
+            tree.setRoot(tree.insertNode(tree.getRoot(), 1));
+            tree.setRoot(tree.insertNode(tree.getRoot(), 2));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         tree.preOrder(tree.getRoot());
 
-        tree.setRoot(tree.deleteNode(tree.getRoot(), 10));
+        try {
+            tree.setRoot(tree.deleteNode(tree.getRoot(), 10));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
         tree.preOrder(tree.getRoot());
 
-        System.out.println(tree.searchNode(tree.getRoot(), 0).getData());
+        try {
+            System.out.println(tree.searchNode(tree.getRoot(), 34).getData());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
